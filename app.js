d23d058100ccaff9986d7ccdbc78620ec5aa91cb
João Vitor <joaovitordealencardasilva@gmail.com>
2022-06-01 11:07:27 -0300

    Configurando o banco de dados

--- app.js
+++ app.js
@@ -1,4 +1,7 @@
 const express = require('express')
+const mongoose = require('mongoose')
+
+require("dotenv").config()
 
 const app = express()
 
@@ -16,4 +19,15 @@ app.get("/", (req, res) => {
 
 })
 
-app.listen(3000, () => console.log("Server está no ar"))
+const DB_USER = process.env.DB_USER
+const DB_PASSWORD = process.env.DB_PASSWORD
+
+mongoose
+    .connect(
+        `mongodb+srv://${DB_USER}:${DB_PASSWORD}@cluster0.1a57gaq.mongodb.net/?retryWrites=true&w=majority`
+    )
+    .then(() =>{
+        console.log('Conectamos ao MongoDB!')
+        app.listen(process.env.PORT || 3000)
+    })
+    .catch((err) => console.log(err))
