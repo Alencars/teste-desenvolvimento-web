a20498c3e90129c755d4ab391604661ae9720277
João Vitor <joaovitordealencardasilva@gmail.com>
2022-06-01 10:31:13 -0300

    Criando usuário pronto

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -1,4 +1,38 @@
 const express = require("express").Router()
-const User = require('../models')
+const { Router } = require("express")
+const User = require('../models/userModel')
+
+Router.post('/', async (req, res) => {
+    const {name, email, password} = req.body
+
+    if(!name) {
+        res.status(422).json({error: 'O nome é obrigatório!'})
+        return
+    }
+    if(!email) {
+        res.status(422).json({error: 'O nome é obrigatório!'})
+        return
+    }
+    if(!password) {
+        res.status(422).json({error: 'O nome é obrigatório!'})
+        return
+    }
+
+    const user = {
+        name,
+        email,
+        senha
+    }
+
+    try{
+        await User.create(user)
+
+        res.status(201).json({ message: 'Usuário criado com sucesso'})
+    } catch (error) {
+        res.status(500).json({error: error})
+    }
+
+    
+})
 
 
